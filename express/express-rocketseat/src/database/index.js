a62fe0ab822f90f06387b2b80ca188c3c37e9eb1
Vinicius Souza <vineasouza33@hotmail.com>
2020-03-25 02:24:12 -0300

    feat: add codigo API rest, porém com erro no POST

---
+++ express/express-rocketseat/src/database/index.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+mongoose.createConnection('mongodb://localhost/noderest', { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  useCreateIndex: true 
+})
+.then(response => console.log('Conected to Database..'))
+.catch(error => console.log('error ->', error.message));
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
