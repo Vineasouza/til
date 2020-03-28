223fd50e9f84fb9e586d327d330574a127149ec5
Vinicius Souza <vineasouza33@hotmail.com>
2020-03-27 21:07:00 -0300

    feat:  Recuperação de senha com NodeMailer

--- express/express-rocketseat/src/app/controllers/authController.js
+++ express/express-rocketseat/src/app/controllers/authController.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const mailer = require('../../modules/mailer');
 
 const authConfig = require('../../config/auth');
 
@@ -34,7 +36,7 @@ router.post('/register', async(req, res) => {
     }
 });
 
-router.post('/authenticate', async (req, res) => {
+router.post('/authenticate', async(req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email }).select('+password');
@@ -52,6 +54,75 @@ router.post('/authenticate', async (req, res) => {
         user, 
         token: generateToken({ id: user.id})
     });
+});
+
+router.post('/forgot_password', async(req, res) => {
+    const {email} = req.body;
+
+    try {
+        
+        const user = await User.findOne({ email });
+
+        if(!user)
+            return res.status(400).send({ error: 'User not found' });
+
+        const token = crypto.randomBytes(20).toString('hex');
+
+        const now = new Date();
+        now.setHours(now.getHours() + 1);
+
+        await User.findByIdAndUpdate(user.id, {
+            '$set': {
+                passwordResetToken: token,
+                passwordRestExpires: now,
+            }
+        });
+
+        mailer.sendMail({
+            to: email,
+            from: 'email35@hotmail.com',
+            template: '/auth/forgot_password',
+            context: { token },
+        }, (err) => {
+            if(err)
+                return res.status(400).send( { error: 'Cannot send forgot password email'});
+
+            return res.send();
+        }); 
+
+    } catch (err) {
+        console.log(err);
+        res.status(400).send({ error: 'Error on forgot password, try again' });
+    }
+});
+
+router.post('/reset_password', async(req, res) => {
+    const { email, token, password } = req.body;
+
+    try {
+        
+        const user = await User.findOne({ email })
+        .select('+passwordResetToken passwordResetExpires');
+
+        if(!user)
+            return res.status(400).send({ error: 'User not found' });
+        
+        if(token !== user.passwordResetToken)
+            return res.status(400).send({ error: 'Token invalid'});
+        
+        const now = new Date();
+        if(now > user.passwordResetExpires)
+            return res.status(400).send({ error: 'Token expired, generate a new one' });
+
+        user.password = password;
+
+        await user.save();
+
+        res.send();
+
+    } catch (err) {
+        return res.status(400).send( { error: 'Cannot send forgot password, try again' });
+    }
 });
 
 module.exports = app => app.use('/auth', router);
