de9f591bdd8f25a808b9719381e93e97fc84d3ce
Vinicius Souza <vineasouza33@hotmail.com>
2020-04-08 01:29:53 -0300

    feat: adicioandno algumas funcionalidades

--- twitterbot/index.js
+++ twitterbot/index.js
@@ -9,19 +9,21 @@ const bot = new Twit({
     timeout_ms: 60 * 1000,
 });
 
-// tweet something
+//  tweet something  //
 bot.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-    console.log(data)
+    // console.log(err);
+    console.log(data);
+    // console.log(response);
 });
 
-
-
-
-
-
-//  search twitter for all tweets containing the word ' ' since ...
+//  search twitter for all tweets containing the word ' ' since ...  //
 bot.get('search/tweets', { q: 'babu since:2020-03-30', count: 2 }, function(err, data, response) {
     console.log(data)
 })
+
+//  get the list of user id's that follow @...  //
+bot.get('followers/ids', { screen_name: 'vineasouza' },  function (err, data, response) {
+    console.log(data)
+  })
 
 
