0004b34810b6e4ce36a156b7691927ecc7339a8a
Vinicius Souza <vineasouza33@hotmail.com>
2020-04-14 16:26:40 -0300

    feat: adicionando escrita em arquivos txt e setInterval

--- python/covid-19-count/covidBrazil.py
+++ python/covid-19-count/covidBrazil.py
@@ -1,11 +1,24 @@
 import requests 
 from bs4 import BeautifulSoup
+from threading import Timer
 
-url = "https://www.worldometers.info/coronavirus/country/brazil/"
-r = requests.get(url)
-s = BeautifulSoup(r.text,"html.parser")
-data = s.find_all("div",class_ = "maincounter-number")
+def covid():
+    url = "https://www.worldometers.info/coronavirus/country/brazil/"
+    r = requests.get(url)
+    s = BeautifulSoup(r.text,"html.parser")
+    data = s.find_all("div",class_ = "maincounter-number")
 
-print("Total Cases:",data[0].text.strip())
-print("Total Deaths:",data[1].text.strip ())
-print("Total Recovered:",data[2].text.strip())
+    print("---Brazil Stats---")
+    print("Total Cases:",data[0].text.strip())
+    print("Total Deaths:",data[1].text.strip ())
+    print("Total Recovered:",data[2].text.strip())
+    print("\n ----- \n ")
+
+    w = open("covidBrazil.txt", "w")
+    w.write("Total Cases: "+ data[0].text.strip() + "\n")
+    w.write("Total Deaths: " + data[1].text.strip () + "\n")
+    w.write("Total Recovered: " + data[2].text.strip() + "\n")
+    w.close()
+    Timer(60.0, covid).start()
+
+Timer(60.0, covid).start()
