8ab7ab5d9aaa4d9501c4e5e4a159d96da53ab212
Vinicius Souza <vineasouza33@hotmail.com>
2020-05-20 11:29:18 -0300

    feat: adicinando novas funções

--- python/covid-19-count/test.py
+++ python/covid-19-count/test.py
@@ -10,22 +10,11 @@ r = requests.get(url)
 s = BeautifulSoup(r.text,"html.parser")
 data_name = s.find_all("div",class_ = "number-table-main")
 data_name2 = s.find_all("span",class_ = "number-table")
-data_name3 = s.find_all("div",class_ = "panel-front")
-#data_name4 = data_name3[0].find_all("strong")
+
+def data1():
+    print("Casos ativos\nAtualmente infectados: " + data_name[0].text.strip() + "\nEm condições suaves: " + data_name2[0].text.strip() + " (" + data_name2[0].next_sibling.next_sibling.text.strip() + "%)\nSério ou Crítico: " + data_name2[1].text.strip() + " (" + data_name2[1].next_sibling.next_sibling.text.strip() + "%)\n\nCasos fechados\nCasos que tiveram um resultado: " + data_name[1].text.strip() + "\nRecuperados: " + data_name2[2].text.strip() + " (" + data_name2[2].next_sibling.next_sibling.text.strip() + "%)\nMortos: " + data_name2[3].text.strip() + " (" + data_name2[3].next_sibling.next_sibling.text.strip() + "%)")
+    Timer(5.0, data1).start()
 
 
-print("\nDATA1")
-print(data_name[0].text.strip())
-print(data_name2[0].text.strip())
-print(data_name2[1].text.strip())
-print(data_name3)
-
-print("\nDATA2")
-print(data_name[1].text.strip())
-print(data_name2[2].text.strip())
-print(data_name2[3].text.strip())
-
-
-
-
+Timer(5.0, data1).start()
 
