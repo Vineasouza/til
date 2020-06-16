1fc8bdc702147d581afac77c2dcac5aa7c6e64d1
Vinicius Souza <vineasouza33@hotmail.com>
2020-06-15 22:38:15 -0300

    feat: delete date.py

--- python/covidTwitterBot/date.py
+++
@@ -1,8 +0,0 @@
-import datetime
-from pytz import timezone
-
-now = datetime.datetime.now()
-fuso_horario = timezone('America/Sao_Paulo')
-nowtime = now.astimezone(fuso_horario)
-
-print(nowtime.strftime("%Y-%m-%d %H:%M"))
