51a57f16d91639b75d626173ba6d479ca0bd28eb
Vinicius Souza <vineasouza33@hotmail.com>
2020-08-14 00:24:06 -0300

    feat: add meme

--- bash/dwnld.sh
+++ bash/dwnld.sh
@@ -21,16 +21,24 @@ then
         perc=$(((step + 1) * 100 / MAX_STEPS))
         # calculate the last bar characters at percentage
         percBar=$((perc * MAX_BAR_SIZE / 100))
-        
+
         sleep 1
 
         # show the current loop percent
         # flag -n keep the pointer on the same line
         # flag -e return the pointer at the beginning of the line
-        echo -ne "\\r[UPDATE ${BAR_SIZE:0:percBar}] $perc %"
+        echo -ne "\\r[${BAR_SIZE:0:percBar}] $perc %"
 
         
     done
+    echo ""
+    echo -n "( •_•)"
+    sleep .75
+    echo -n -e "\r( •_•)>⌐■-■"
+    sleep .75
+    echo -n -e "\r               "
+    echo  -e "\r(⌐■_■)"
+    sleep .5
     echo ""
     # back the cursor
     tput cnorm -- inormal
