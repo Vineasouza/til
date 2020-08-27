7607b83b18a7294dd28b77fa5d977aaf3cd28695
Vinicius Souza <vineasouza33@hotmail.com>
2020-08-27 10:10:35 -0300

    feat: add some tests with textfield material-ui

---
+++ javascript/react-test/src/Test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import './Test.css';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+    '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '454px',
+        borderColor: "#08082D",
+        borderWidth: '7px',     
+    },
+    },
+}));
+
+export default function FormPropsTextFields() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+        <div>
+            <TextField
+            required
+            id="outlined-required"
+            label="Nome"
+            defaultValue=" "
+            variant="outlined"
+            helperText="Escreva seu nome completo"
+            input
+            InputProps={{
+                className: classes.input
+            }}
+            />
+        </div>
+        <div>
+            <TextField
+            required
+            id="outlined-required"
+            label="Especialidade"
+            defaultValue=" "
+            variant="outlined"
+            helperText="Sua área de especialidade"
+            />
+        </div>
+        <div>
+            <TextField
+            required
+            id="outlined-required"
+            label="Convênio"
+            defaultValue=" "
+            variant="outlined"
+            helperText="Todos os convênios atendidos"
+            />
+        </div>
+    </form>
+  );
+}
