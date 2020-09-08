f1e1fd03405a0747406b3f8901c684982acb6784
Vinicius Souza <vineasouza33@hotmail.com>
2020-09-08 11:21:03 -0300

    update 08/09/2020

---
+++ javascript/react-test/src/TestGeo.jsx
@@ -0,0 +1,26 @@
+import Geocode from "react-geocode";
+import React from  'react'
+
+Geocode.setApiKey("AIzaSyDzVYTrAJKeR3FN4dxOgtvKcrsdUKIWDx4");
+Geocode.enableDebug();
+
+Geocode.fromLatLng("48.8583701", "2.2922926").then(
+  response => {
+    const address = response.results[0].formatted_address;
+    console.log(address);
+  },
+  error => {
+    console.error(error);
+  }
+);
+
+function Geolocator() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+export default Geolocator;
--- javascript/react-test/src/index.js
+++ javascript/react-test/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TestGeo from './TestGeo';
 import './index.css';
-import Test from './Test2';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Test/>
+    <TestGeo/>
   </React.StrictMode>,
   document.getElementById('root')
 );
