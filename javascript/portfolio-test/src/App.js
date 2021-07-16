24d367a1a80a3f247f2ca48e492330fc11296c4f
Vineasouza <vineasouza33@hotmail.com>
2021-07-15 22:01:08 -0300

    update

--- javascript/portfolio-test/src/App.js
+++ javascript/portfolio-test/src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Fade from 'react-reveal/Fade';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Fade right>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </Fade>
+        <Fade left>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </Fade>
       </header>
     </div>
   );
